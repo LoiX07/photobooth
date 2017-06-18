182f8763faa1ee2521102585c01e073deebbea7e
Loïc Maudou <loic.maudou_git@polytechnique.org>
2017-06-18 20:52:20 +0200

    Structure du programme

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 # Imports
+import RPi.GPIO as GPIO
 from time import sleep
 from picamera import PiCamera
+
+
+# Constante
+versionCamera = 1 # 1 ou 2 en fonction de la version de la pi camera utilisée
 
 # Initialisation
 camera = PiCamera()
 
 # Configuration
-camera.resolution = (1024, 768)
+if versionCamera==1
+    camera.resolution = (1024, 768)
+elif versionCamera==2
+    camera.resolution = (3280,2464)
 
+# Exception pour quitter la boucle
 
 camera.start_preview()
 # Camera warm-up time
 sleep(2)
 camera.capture('foo.jpg')
+
+# Eclairage en attente
+# While true
+    # Compte à rebourd
+    # Eclairage plus puissant
+    # Prise de la photo
 

4d2080aa8cee5721d08c0fd9af9d180e435ed233
tonio213 <me@frescriva.com>
2017-07-11 18:35:15 +0200

    PEP8 compliance

--- photobooth.py
+++ photobooth.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # Imports
 import exceptions
@@ -7,38 +8,47 @@ import sys
 from time import sleep
 from picamera import PiCamera
 
-
 ##################
 ### Parameters ###
 ##################
 gpio_lamp_channel = 18
 gpio_trigger_channel = 23
 gpio_shutdown_channel = 24
-gpio_7segments_display = {"A" : 2, "B" : 3, "C" : 4, "D" : 17, "E" : 27, "F" : 22, "G" : 5} 
-gpio_shutdown_led_channel = 12 
+gpio_7segments_display = {
+    "A": 2,
+    "B": 3,
+    "C": 4,
+    "D": 17,
+    "E": 27,
+    "F": 22,
+    "G": 5
+}
+gpio_shutdown_led_channel = 12
 gpio_trigger_led_channel = 25
 picture_path = datetime.now().strftime("%Y-%m-%d_Photomaton")
 picture_basename = "%H-%M-%S_Photomaton.jpeg"
-typeCamera = 1 # 1 for raspberry pi camera, 2 for a reflex camera
-versionCamera = 1 # 1 or 2 depending of the camera version
+typeCamera = 1  # 1 for raspberry pi camera, 2 for a reflex camera
+versionCamera = 1  # 1 or 2 depending of the camera version
 
 #####################
 ### Configuration ###
 #####################
-wiringpi.wiringPiSetupGpio() # use wiringpi numerotation
+wiringpi.wiringPiSetupGpio()  # use wiringpi numerotation
+
 
 ###############
 ### Classes ###
 ###############
 class Camera:
     """ Objet camera """
-    
+
     def prepareCamera(self):
         """ Camera initialization """
         raise NotImplemented
 
-    def takeAPicture(self,path):
+    def takeAPicture(self, path):
         raise NotImplemented
+
 
 class Raspicam(Camera):
     """ Camera Raspberry """
@@ -47,25 +57,27 @@ class Raspicam(Camera):
         """ Initialization of the camera """
         self.camera = PiCamera()
         # Configuration
-        if versionCamera==1:
+        if versionCamera == 1:
             camera.resolution = (1024, 768)
-        elif versionCamera==2:
-            camera.resolution = (3280,2464)
+        elif versionCamera == 2:
+            camera.resolution = (3280, 2464)
         # Camera warm-up
         camera.start_preview()
         sleep(2)
-    
+
     def prepareCamera(self):
         """ Camera preparation """
 
-    def takeAPicture(self,picture_path,picture_basename):
+    def takeAPicture(self, picture_path, picture_basename):
         """ Take a picture with the camera """
-        camera.capture(picture_path + "/" + datetime.now().strftime(picture_basename))
+        camera.capture(picture_path + "/" +
+                       datetime.now().strftime(picture_basename))
 
     def close():
         camera.stop_preview()
         """ Free the camera ressources to avoid GPU memory leaks """
         camera.close()
+
 
 class ReflexCam(Camera):
     """ REFLEX Camera """
@@ -76,57 +88,70 @@ class ReflexCam(Camera):
     def prepareCamera(self):
         """ Prepare the camera for the picture """
 
-    def takeAPicture(self,path):
+    def takeAPicture(self, path):
         """ Take a picture with the camera """
 
     def close():
         """ Free the camera ressources to avoid GPU memory leaks """
 
+
 class Lamp:
     """ Lighting for the Photobooth """
+
     def __init__(channel):
         """ Initialization """
         self.channel = channel
-        wiringpi.pinMode(channel,2)
-        wiringpi.pwmSetMode(channel,0)
-    
+        wiringpi.pinMode(channel, 2)
+        wiringpi.pwmSetMode(channel, 0)
+
     def idle():
         """ Set the lights to idle level """
-        wiringpi.pwmWrite(0.1*1024)
+        wiringpi.pwmWrite(0.1 * 1024)
 
     def setLevel(level):
         """ Lighting coefficient modification between 0 and 1 """
-        wiringpi.pwmWrite(self.channel,level*1024)    
+        wiringpi.pwmWrite(self.channel, level * 1024)
+
 
 class CountDisplay:
     """ 7 segment display """
-    
+
     def __init__(channels):
         """ Initialization of the 7 segment display """
         self.channels = channels
         # Pins configuration "
         for char in "ABCDEFG":
-            wiringpi.pinMode(self.channels[char],1)
-            wiringpi.digitalWrite(self.channels[char],1)
+            wiringpi.pinMode(self.channels[char], 1)
+            wiringpi.digitalWrite(self.channels[char], 1)
 
     def display(number):
         """ Display the requested number """
-        wiringpi.digitalWrite(self.channels['A'], not number in [0,2,3,5,6,7,8,9])
-        wiringpi.digitalWrite(self.channels['B'], not number in [0,1,2,3,4,7,8,9])
-        wiringpi.digitalWrite(self.channels['C'], not number in [0,1,3,4,5,6,7,8,9])
-        wiringpi.digitalWrite(self.channels['D'], not number in [0,2,3,5,6,8,9])
-        wiringpi.digitalWrite(self.channels['E'], not number in [0,2,6,8])
-        wiringpi.digitalWrite(self.channels['F'], not number in [0,4,5,6,8,9])
-        wiringpi.digitalWrite(self.channels['G'], not number in [2,3,4,5,6,8,9])
+        wiringpi.digitalWrite(self.channels['A'],
+                              not number in [0, 2, 3, 5, 6, 7, 8, 9])
+        wiringpi.digitalWrite(self.channels['B'],
+                              not number in [0, 1, 2, 3, 4, 7, 8, 9])
+        wiringpi.digitalWrite(self.channels['C'],
+                              not number in [0, 1, 3, 4, 5, 6, 7, 8, 9])
+        wiringpi.digitalWrite(self.channels['D'],
+                              not number in [0, 2, 3, 5, 6, 8, 9])
+        wiringpi.digitalWrite(self.channels['E'], not number in [0, 2, 6, 8])
+        wiringpi.digitalWrite(self.channels['F'],
+                              not number in [0, 4, 5, 6, 8, 9])
+        wiringpi.digitalWrite(self.channels['G'],
+                              not number in [2, 3, 4, 5, 6, 8, 9])
 
     def switchOff():
         for char in "ABCDEF":
-            wiringpi.digitalWrite(self.channels[char],1) # swith off the segment
+            wiringpi.digitalWrite(self.channels[char],
+                                  1)  # swith off the segment
+
 
 class Photobooth:
     """ Photobooth """
-    
-    def __init__(self,picture_path,picture_basename,picture_size,trigger_channel, trigger_led_channel, seven_segments_channels, shutdown_channel, shutdown_led_channel, lamp_channel):
+
+    def __init__(self, picture_path, picture_basename, picture_size,
+                 trigger_channel, trigger_led_channel, seven_segments_channels,
+                 shutdown_channel, shutdown_led_channel, lamp_channel):
         """ Initialization """
         # Initialize the parameters
         self.picture_path = picture_path
@@ -144,16 +169,16 @@ class Photobooth:
 
         # Switch on the lights
         self.lamp.idle()
-        wiringpi.digitalWrite(self.trigger_led_channel,1)
-        wiringpi.digitalWrite(self.shutdown_led_channel,1)
+        wiringpi.digitalWrite(self.trigger_led_channel, 1)
+        wiringpi.digitalWrite(self.shutdown_led_channel, 1)
 
         # Events detection
-        wiringpi.pinMode(trigger_channel,0)
-        wiringpi.pullUpDnControl(trigger_channel,1)
-        wiringpi.wiringPiISR(trigger_channel,2,takePicture)
-        wiringpi.pinMode(shutdown_channel,0)
-        wiringpi.pullUpDnControl(shutdown_channel,1)
-        wiringpi.wiringPiISR(trigger_shutdown,2,quit)
+        wiringpi.pinMode(trigger_channel, 0)
+        wiringpi.pullUpDnControl(trigger_channel, 1)
+        wiringpi.wiringPiISR(trigger_channel, 2, takePicture)
+        wiringpi.pinMode(shutdown_channel, 0)
+        wiringpi.pullUpDnControl(shutdown_channel, 1)
+        wiringpi.wiringPiISR(trigger_shutdown, 2, quit)
 
         # semaphore on picture taking (to ignore a second clic during a taking picture sequence)
         self.takingPicture = False
@@ -162,34 +187,39 @@ class Photobooth:
         """ Launch the photo sequence """
         if self.takingPicture == False:
             self.takingPicture = True
-            wiringpi.digitalWrite(self.shutdown_led_channel,0)
+            wiringpi.digitalWrite(self.shutdown_led_channel, 0)
             self.camera.preparation()
-            for i in xrange(5,0,-1):
-                self.lamp.setLevel((5-i)/5) # Progressive increase of the lights
-                self.countDisplay.display(i) # Countdown update
+            for i in xrange(5, 0, -1):
+                self.lamp.setLevel(
+                    (5 - i) / 5)  # Progressive increase of the lights
+                self.countDisplay.display(i)  # Countdown update
                 if i != 0:
                     sleep(1)
-            # Take a picture    
-            self.camera.takePicture(self.picture_path,self.picture_basename)
-            sleep(1) #TODO : to adjust
+            # Take a picture
+            self.camera.takePicture(self.picture_path, self.picture_basename)
+            sleep(1)  #TODO : to adjust
             self.lamp.idle()
             self.countDisplay.switchOff()
-            wiringpi.digitalWrite(self.shutdown_led_channel,1)
+            wiringpi.digitalWrite(self.shutdown_led_channel, 1)
             self.takingPicture = False
 
     def quit(self):
         self.camera.close()
         self.lamp.level(0)
         self.countDisplay.switchOff()
-        wiringpi.digitalWrite(self.trigger_led_channel,0)
-        wiringpi.digitalWrite(self.shutdown_led_channel,0)
+        wiringpi.digitalWrite(self.trigger_led_channel, 0)
+        wiringpi.digitalWrite(self.shutdown_led_channel, 0)
         sys.exit()
 
 
 def main():
-    Photobooth(picture_path,picture_basename,picture_size,gpio_trigger_channel,gpio_trigger_led_channel,gpio_7segments_display,gpio_shutdown_channel,gpio_shutdown_led_channel,gpio_lamp_channel)
+    Photobooth(picture_path, picture_basename, picture_size,
+               gpio_trigger_channel, gpio_trigger_led_channel,
+               gpio_7segments_display, gpio_shutdown_channel,
+               gpio_shutdown_led_channel, gpio_lamp_channel)
     while True:
         sleep(10)
+
 
 if __name__ == "__main__":
     exit(main())
--- remoteCtrl.py
+++ remoteCtrl.py
@@ -5,7 +5,7 @@ import os
 import threading
 import sys
 import pygame
-from time import sleep,clock
+from time import sleep, clock
 from datetime import datetime
 
 ##################
@@ -13,9 +13,9 @@ from datetime import datetime
 ##################
 picture_path = datetime.now().strftime("%Y-%m-%d_Photomaton")
 picture_suffix = "_Photomaton.jpeg"
-slideshow_directory = "test/" #TODO 
-display_size = (1920,1080) #(800,480)
-display_time = 1 #TODO 5
+slideshow_directory = "test/"  #TODO
+display_size = (1920, 1080)  #(800,480)
+display_time = 1  #TODO 5
 
 #####################
 ### Configuration ###
@@ -24,12 +24,12 @@ os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_MOUSEDRV', 'TSLIB')
 os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
 
+
 ###############
 ### Classes ###
 ###############
 class PictureList:
-
-    def __init__(self,path,suffix):
+    def __init__(self, path, suffix):
         """Initialize filenames to the given basename and search for existing files.
         Set the counter accordingly"""
 
@@ -41,7 +41,7 @@ class PictureList:
 
         # Find existing files
         self.pictures = find_pictures()
-        
+
         # Sort picture
         self.pictures.sort()
 
@@ -53,10 +53,11 @@ class PictureList:
         """ Get the sorted list of picture """
         return self.pictures
 
+
 class GUIModule:
     """ GUI Display using PyGame """
 
-    def __init__(self,name,size):
+    def __init__(self, name, size):
         # Call init routines
         pygame.init()
 
@@ -74,7 +75,7 @@ class GUIModule:
         self.clear()
         self.apply()
 
-    def clear(self, color=(46,52,54)):
+    def clear(self, color=(46, 52, 54)):
         self.screen.fill(color)
         self.surface_list = []
 
@@ -86,69 +87,86 @@ class GUIModule:
     def get_size(self):
         return self.size
 
-    def show_picture(self, filename, size=(0,0), offset=(0,0), flip=False):
+    def show_picture(self, filename, size=(0, 0), offset=(0, 0), flip=False):
         # Use window size if none given
-        if size == (0,0):
+        if size == (0, 0):
             size = self.size
         try:
             # Load image from file
             image = pygame.image.load(filename)
         except pygame.error as e:
-            raise GuiException("ERROR: Can't open image '" + filename + "': " + e.message)
+            raise GuiException("ERROR: Can't open image '" + filename + "': " +
+                               e.message)
         # Extract image size and determine scaling
         image_size = image.get_rect().size
-        image_scale = min([min(a,b)/b for a,b in zip(size, image_size)])
+        image_scale = min([min(a, b) / b for a, b in zip(size, image_size)])
         # New image size
-        new_size = [int(a*image_scale) for a in image_size]
+        new_size = [int(a * image_scale) for a in image_size]
         # Update offset
-        offset = tuple(a+int((b-c)/2) for a,b,c in zip(offset, size, new_size))
+        offset = tuple(a + int((b - c) / 2)
+                       for a, b, c in zip(offset, size, new_size))
         # Apply scaling and display picture
         image = pygame.transform.scale(image, new_size).convert()
         # Create surface and blit the image to it
         surface = pygame.Surface(new_size)
-        surface.blit(image, (0,0))
+        surface.blit(image, (0, 0))
         if flip:
             surface = pygame.transform.flip(surface, True, False)
         self.surface_list.append((surface, offset))
 
-    def show_message(self, msg, color=(0,0,0), bg=(230,230,230), transparency=True, outline=(245,245,245)):
+    def show_message(self,
+                     msg,
+                     color=(0, 0, 0),
+                     bg=(230, 230, 230),
+                     transparency=True,
+                     outline=(245, 245, 245)):
         # Choose font
         font = pygame.font.Font(None, 144)
         # Wrap and render text
         wrapped_text, text_height = self.wrap_text(msg, font, self.size)
-        rendered_text = self.render_text(wrapped_text, text_height, 1, 1, font, color, bg, transparency, outline)
+        rendered_text = self.render_text(wrapped_text, text_height, 1, 1, font,
+                                         color, bg, transparency, outline)
 
-        self.surface_list.append((rendered_text, (0,0)))
+        self.surface_list.append((rendered_text, (0, 0)))
 
-    def show_button(self, text, pos, size=(0,0), color=(230,230,230), bg=(0,0,0), transparency=True, outline=(230,230,230)):
+    def show_button(self,
+                    text,
+                    pos,
+                    size=(0, 0),
+                    color=(230, 230, 230),
+                    bg=(0, 0, 0),
+                    transparency=True,
+                    outline=(230, 230, 230)):
         # Choose font
         font = pygame.font.Font(None, 72)
         text_size = font.size(text)
-        if size == (0,0):
+        if size == (0, 0):
             size = (text_size[0] + 4, text_size[1] + 4)
-        offset = ( (size[0] - text_size[0]) // 2, (size[1] - text_size[1]) // 2 )
+        offset = ((size[0] - text_size[0]) // 2, (size[1] - text_size[1]) // 2)
 
         # Create Surface object and fill it with the given background
-        surface = pygame.Surface(self.size) 
-        surface.fill(bg) 
+        surface = pygame.Surface(self.size)
+        surface.fill(bg)
 
         # Render text
         rendered_text = font.render(text, 1, color)
         surface.blit(rendered_text, pos)
 
         # Render outline
-        pygame.draw.rect(surface, outline, (pos[0]-offset[0], pos[1]-offset[0], size[0], size[1]), 1)
+        pygame.draw.rect(surface, outline,
+                         (pos[0] - offset[0], pos[1] - offset[0], size[0],
+                          size[1]), 1)
 
         # Make background color transparent
         if transparency:
             surface.set_colorkey(bg)
 
-        self.surface_list.append((surface, (0,0)))
+        self.surface_list.append((surface, (0, 0)))
 
     def wrap_text(self, msg, font, size):
-        final_lines = []                   # resulting wrapped text
-        requested_lines = msg.splitlines() # wrap input along line breaks
-        accumulated_height = 0             # accumulated height
+        final_lines = []  # resulting wrapped text
+        requested_lines = msg.splitlines()  # wrap input along line breaks
+        accumulated_height = 0  # accumulated height
 
         # Form a series of lines
         for requested_line in requested_lines:
@@ -165,9 +183,9 @@ class GUIModule:
                 # Put words on the line as long as they fit
                 for word in words:
                     test_line = accumulated_line + word + " "
-                    # Build the line while the words fit.   
+                    # Build the line while the words fit.
                     if font.size(test_line)[0] < size[0]:
-                        accumulated_line = test_line 
+                        accumulated_line = test_line
                     else:
                         # Start a new line
                         line_height = font.size(accumulated_line)[1]
@@ -176,7 +194,7 @@ class GUIModule:
                         else:
                             accumulated_height += line_height
                             final_lines.append(accumulated_line)
-                            accumulated_line = word + " " 
+                            accumulated_line = word + " "
                 # Finish requested_line
                 line_height = font.size(accumulated_line)[1]
                 if accumulated_height + line_height > size[1]:
@@ -186,7 +204,7 @@ class GUIModule:
                     final_lines.append(accumulated_line)
             # Line fits as it is
             else:
-                accumulated_height += font.size(requested_line)[1] 
+                accumulated_height += font.size(requested_line)[1]
                 final_lines.append(requested_line)
 
         # Check height of wrapped text
@@ -195,40 +213,42 @@ class GUIModule:
 
         return final_lines, accumulated_height
 
-    def render_text(self, text, text_height, valign, halign, font, color, bg, transparency, outline):
+    def render_text(self, text, text_height, valign, halign, font, color, bg,
+                    transparency, outline):
         # Determine vertical position
-        if valign == 0:     # top aligned
+        if valign == 0:  # top aligned
             voffset = 0
-        elif valign == 1:   # centered
+        elif valign == 1:  # centered
             voffset = int((self.size[1] - text_height) / 2)
-        elif valign == 2:   # bottom aligned
+        elif valign == 2:  # bottom aligned
             voffset = self.size[1] - text_height
         else:
             raise GuiException("Invalid valign argument: " + str(valign))
 
         # Create Surface object and fill it with the given background
-        surface = pygame.Surface(self.size) 
-        surface.fill(bg) 
+        surface = pygame.Surface(self.size)
+        surface.fill(bg)
 
         # Blit one line after another
-        accumulated_height = 0 
-        for line in text: 
+        accumulated_height = 0
+        for line in text:
             maintext = font.render(line, 1, color)
             shadow = font.render(line, 1, outline)
-            if halign == 0:     # left aligned
+            if halign == 0:  # left aligned
                 hoffset = 0
-            elif halign == 1:   # centered
+            elif halign == 1:  # centered
                 hoffset = (self.size[0] - maintext.get_width()) / 2
-            elif halign == 2:   # right aligned
+            elif halign == 2:  # right aligned
                 hoffset = rect.width - maintext.get_width()
             else:
-                raise GuiException("Invalid halign argument: " + str(justification))
+                raise GuiException("Invalid halign argument: " +
+                                   str(justification))
             pos = (hoffset, voffset + accumulated_height)
             # Outline
-            surface.blit(shadow, (pos[0]-1,pos[1]-1))
-            surface.blit(shadow, (pos[0]-1,pos[1]+1))
-            surface.blit(shadow, (pos[0]+1,pos[1]-1))
-            surface.blit(shadow, (pos[0]+1,pos[1]+1))
+            surface.blit(shadow, (pos[0] - 1, pos[1] - 1))
+            surface.blit(shadow, (pos[0] - 1, pos[1] + 1))
+            surface.blit(shadow, (pos[0] + 1, pos[1] - 1))
+            surface.blit(shadow, (pos[0] + 1, pos[1] + 1))
             # Text
             surface.blit(maintext, pos)
             accumulated_height += font.size(line)[1]
@@ -242,29 +262,31 @@ class GUIModule:
 
     def teardown(self):
         pygame.quit()
-        
+
+
 class Slideshow:
     """ Slideshow : displays pictures in a folder and when a new picture is taken, displays it during a few time and let user choose if he wants to delete it """
-    
-    def __init__(self, display_size, display_time, directory, recursive = True):
-        self.directory    = directory
-        self.recursive    = recursive
-        self.filelist     = []
-        self.display      = GUIModule("Slideshow", display_size)
+
+    def __init__(self, display_size, display_time, directory, recursive=True):
+        self.directory = directory
+        self.recursive = recursive
+        self.filelist = []
+        self.display = GUIModule("Slideshow", display_size)
         self.display_time = display_time
-        self.next         = 0
+        self.next = 0
         self.time_before_next = display_time
         self.scrolling = True
         self.quitting = False
         self.step = 0.1
         self.monitoringThread = threading.Thread(target=self.monitorEvents)
-        self.monitoringThread.start() # Run
+        self.monitoringThread.start()  # Run
 
     def scan(self):
         filelist = []
         if self.recursive:
             # Recursively walk all entries in the directory
-            for root, dirnames, filenames in os.walk(self.directory, followlinks=True):
+            for root, dirnames, filenames in os.walk(
+                    self.directory, followlinks=True):
                 for filename in filenames:
                     filelist.append(os.path.join(root, filename))
         else:
@@ -276,7 +298,6 @@ class Slideshow:
 
         self.filelist = filelist
         self.next = 0
-
 
     def display_next(self, text=""):
         if self.next >= len(self.filelist):
@@ -304,7 +325,7 @@ class Slideshow:
             self.handle_event(pygame.event.wait())
 
     def run(self):
-        while not self.quitting :
+        while not self.quitting:
             self.display_next()
             while self.time_before_next > 0 and self.scrolling and not self.quitting:
                 sleep(self.step)
@@ -321,7 +342,7 @@ class Slideshow:
     def get_size(self):
         return self.size
 
-    def handle_key_pressed(self,key):
+    def handle_key_pressed(self, key):
         """ Handle a pressed key """
         if key == pygame.constants.K_q:
             self.teardown()
@@ -334,32 +355,38 @@ class Slideshow:
         self.quitting = True
         self.display.teardown()
 
+
 #################
 ### Functions ###
 #################
 
+
 def sync_folders(source_directory, target_directory, wait_time):
     sleep(5)
     while True:
-        print("[" + datetime.now().strftime("%H:%M:%S") + "] Sync " 
-                + source_directory + " --> " + target_directory)
+        print("[" + datetime.now().strftime("%H:%M:%S") + "] Sync " +
+              source_directory + " --> " + target_directory)
         try:
             cmd = "rsync -rtu " + source_directory + " " + target_directory
-            output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+            output = subprocess.check_output(
+                cmd, shell=True, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             print("ERROR executing '" + e.cmd + "':\n" + e.output)
         sleep(wait_time)
+
 
 def main():
     # Start a thread for syncing files
     #if len(source_directory) > 0:
     #   thread.start_new_thread(sync_folders, (source_directory, slideshow_directory, sync_time) )
-    
+
     # Start the slideshow
-    slideshow = Slideshow(display_size, display_time, slideshow_directory, True)
+    slideshow = Slideshow(display_size, display_time, slideshow_directory,
+                          True)
     slideshow.run()
 
     return 0
+
 
 if __name__ == "__main__":
     exit(main())

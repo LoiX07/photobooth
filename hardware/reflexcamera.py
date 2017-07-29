3f7099287caa38d7590888e15adcc709fb7302b9
Loïc Maudou <LoiX07@users.noreply.github.com>
2017-07-29 17:29:18 +0200

    GPhoto2 in progress...

--- hardware/reflexcamera.py
+++ hardware/reflexcamera.py
@@ -12,15 +12,26 @@ class ReflexCam(Camera):
     def __init__(self):
         """ Initialization of the camera """
         gp.check_result(gp.use_python_logging())
-        context = gp.gp_context_new()
+        self.context = gp.gp_context_new()
         self.camera = gp.check_result(gp.gp_camera_new())
-        gp.check_result(gp.gp_camera_init(self.camera, context))
+        gp.check_result(gp.gp_camera_init(self.camera, self.context))
 
     def prepare_camera(self):
         """ Prepare the camera for the picture """
 
     def take_picture(self, path, basename):
         """ Take a picture with the camera """
-
+        print('Capturing image')
+        file_path = gp.check_result(gp.gp_camera_capture(
+        camera, gp.GP_CAPTURE_IMAGE, self.context))
+        print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+        target = os.path.join('/tmp', file_path.name)
+        print('Copying image to', target)
+        camera_file = gp.check_result(gp.gp_camera_file_get(
+            self.camera, file_path.folder, file_path.name,
+            gp.GP_FILE_TYPE_NORMAL, context))
+        gp.check_result(gp.gp_file_save(camera_file, target))
+    
     def close(self):
         """ Free the camera ressources to avoid GPU memory leaks """
+        gp.check_result(gp.gp_camera_exit(self.camera,self.context))
